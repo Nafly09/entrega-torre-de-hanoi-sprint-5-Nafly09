5b79d8a6ed77877a7c85f5bb52cbdccec010f42a
Naftaly Junior <juniorinterlyte95@hotmail.com>
2021-08-02 22:42:19 -0300

    feat: Finalizei o Drag and Drop mas apenas no desktop

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-let torre1 = document.getElementById('torre1');
+let discsOnTower1 = [null, null, null, null, null, null, null, null];
+let discsOnTower2 = [null, null, null, null, null, null, null, null];
+let discsOnTower3 = [null, null, null, null, null, null, null, null];
+let boxes = document.querySelectorAll('boxTorres');
+let disks = document.querySelectorAll('div.disco');
+
+function allowDrop(event){
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData('Object', event.target.id)
+}
+
+function drop(event, el){
+    event.preventDefault();
+    let data = event.dataTransfer.getData('Object');
+    el.prepend(document.getElementById(data))
+}
