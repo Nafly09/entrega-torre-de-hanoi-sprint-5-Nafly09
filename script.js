fda39706ede34cba1107483c5d0234e37b15f135
Naftaly Junior <juniorinterlyte95@hotmail.com>
2021-08-03 10:18:27 -0300

    feat: finalizacao da versao desktop do movimentacao dos discos

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let discsOnTower1 = [null, null, null, null, null, null, null, null];
-let discsOnTower2 = [null, null, null, null, null, null, null, null];
-let discsOnTower3 = [null, null, null, null, null, null, null, null];
+// let discsOnTower1 = [null, null, null, null, null, null, null, null];
+// let discsOnTower2 = [null, null, null, null, null, null, null, null];
+// let discsOnTower3 = [null, null, null, null, null, null, null, null];
+
 let boxes = document.querySelectorAll('boxTorres');
 let disks = document.querySelectorAll('div.disco');
 
+// FUNÇÃO PARA PERMITIR QUE O DROP ACONTEÇA
 function allowDrop(event){
     event.preventDefault();
 }
 
+// FUNÇÃO PARA PEGAR OS DADOS DO OBJETO ARRASTADO PELO MOUSE
 function drag(event) {
     event.dataTransfer.setData('Object', event.target.id)
 }
 
+// FUNÇÃO PARA JUNTAR O OBJETO ARRASTADO A TORRE DESIGNADA
 function drop(event, el){
     event.preventDefault();
     let data = event.dataTransfer.getData('Object');
