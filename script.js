2cf2ef55f7cf5c0810112e5e0cd3194c0c50f33c
Naftaly Junior <juniorinterlyte95@hotmail.com>
2021-08-03 12:44:21 -0300

    fix: criacao dos elementos pelo DOM

--- script.js
+++ script.js
@@ -2,22 +2,37 @@
 // let discsOnTower2 = [null, null, null, null, null, null, null, null];
 // let discsOnTower3 = [null, null, null, null, null, null, null, null];
 
-let boxes = document.querySelectorAll('boxTorres');
-let disks = document.querySelectorAll('div.disco');
+// PEGANDO OS ELEMENTOS PRO DOM
+let blocoDeDiscos = document.getElementById('blocoDiscos');
+let blocoDeTorres = document.getElementById('blocoTorres');
+let blocoBoxTorres = document.getElementById('blocoBoxTorres');
 
-// FUNÇÃO PARA PERMITIR QUE O DROP ACONTEÇA
-function allowDrop(event){
-    event.preventDefault();
+// FUNÇÃO PARA CRIAR OS DISCOS
+function createDiscs(){
+    for(let i = 1; i <= 4; i++){
+        let discos = document.createElement('div');
+        discos.setAttribute('id',`disco${i}`);
+        discos.classList.add('discos');
+        blocoDeDiscos.appendChild(discos)
+    }
+    return blocoDeDiscos
+}
+createDiscs()
+
+
+// FUNÇÃO PARA CRIAR AS TORRES
+function createTowers(){
+    for (let j = 1; j <= 3; j++){
+        let torres = document.createElement('div');
+        let blocos = document.createElement('div');
+        torres.setAttribute('id', `torre${j}`);
+        torres.classList.add('torres');
+        blocos.setAttribute('id', `boxTorre${j}`);
+        blocos.classList.add('boxTorres');
+        blocoBoxTorres.prepend(blocos);
+        blocoDeTorres.appendChild(torres);
+    }
+    return blocoBoxTorres
 }
 
-// FUNÇÃO PARA PEGAR OS DADOS DO OBJETO ARRASTADO PELO MOUSE
-function drag(event) {
-    event.dataTransfer.setData('Object', event.target.id)
-}
-
-// FUNÇÃO PARA JUNTAR O OBJETO ARRASTADO A TORRE DESIGNADA
-function drop(event, el){
-    event.preventDefault();
-    let data = event.dataTransfer.getData('Object');
-    el.prepend(document.getElementById(data))
-}
+createTowers();
